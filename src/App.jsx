bb9b3ae6743132d465c32f2da46fb527309ce734
gourain <ngoutsoprainsong@gmail.com>
2023-06-16 08:30:05 +0100

    some issues fixed on other pages 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import { HashRouter } from "react-router-dom";
 import Router from './router/router'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <HashRouter>
+      <HashRouter basename="/">
         <Router />
       </HashRouter>
     </>
